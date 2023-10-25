8f5a32861245452190b537f0d4bea724979c436f
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-10-25 01:46:04 -0400

    Added font to be used with shapes

--- index.js
+++ index.js
@@ -2,28 +2,40 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const { Triangle, Square, Circle } = require("./lib/shapes")
 
-function writeToFile(filename, answers){
+function writeToFile(fileName, answers) {
     let svgString = "";
 
     svgString =
-    '<svg version="1.1" width="275" height="200" xmins="http://www.w3.org/2000/svg">';
+        '<svg version="1.1" width="275" height="200" xmins="http://www.w3.org/2000/svg">';
 
     svgString += "<g>";
 
-    svgString += `${answers.shape}` ;
-} 
+    svgString += `${answers.shape}`;
+
 
 let shapeOption;
-    if (answers.shape === "Triangle") {
-        shapeOption = new Triangle();
-        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`; 
-    } else if (answers.shape === "Square") {
-        shapeOption = new Square();
-        svgString += `<rest x="70" y="40" width="150" fill="${answers.shapeBackgroundColor}"/>`;
-    } else {
-        shapeOption = new Circle();
-        svgString += `<circle cx="150" cy="110" r="90" fill="${answers.shapeBackgroundColor}"/>`;
-    }
+if (answers.shape === "Triangle") {
+    shapeOption = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+} else if (answers.shape === "Square") {
+    shapeOption = new Square();
+    svgString += `<rect x="70" y="40" width="150" fill="${answers.shapeBackgroundColor}"/>`;
+} else {
+    shapeOption = new Circle();
+    svgString += `<circle cx="150" cy="110" r="90" fill="${answers.shapeBackgroundColor}"/>`;
+}
+
+svgString += `<text x="150" y="120" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+
+svgString += "</g>";
+
+svgString += "</svg>";
+
+fs.writeFile(fileName, svgString, (err) => {
+    err ? console.log(err) : console.log("logo.svg");
+});
+}
+
 
 function questions() {
     inquirer
@@ -48,14 +60,15 @@ function questions() {
             },
         ])
         .then((answers) => {
-            if(answers.text.length > 3) {
-            console.log("Please enter 3 character limit");
-            questions();
-        } else {
-            writeToFile("logo.svg", answers);
-        }
+            if (answers.text.length > 3) {
+                console.log("Please enter 3 character limit");
+                questions();
+            } else {
+                writeToFile("logo.svg", answers);
+            }
         })
-    }
+}
+
 
 
 questions()
