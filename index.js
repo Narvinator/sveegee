72f81601cdfb555fa1683d5ab3d971cb8a4a3c1e
NarvinBlas@gmail.com <NarvinBlas@gmail.com>
2023-10-24 23:43:18 -0400

    Created questions and got inquirer running

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 
+function questions() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "Chose the shape of your logo",
+                choices: ["Triangle", "Square", "Circle"],
+                name: "shape",
+            },
+            {
+                type: "input",
+                message:
+                    "Include any characters on the shape? (Three Character Limit)",
+                name: "text",
+            },
+            {
+                type: "input",
+                message:
+                    "Choose text color (Enter color keyword OR a hexadecimal number)",
+                name: "shapeBackgroundColor",
+            },
+        ])
+        .then((answers) => {
+            if(answers.text.length > 3) {
+            console.log("Please enter 3 character limit");
+            questions();
+        } else {
+            writeToFile("logo.svg", answers);
+        }
+        })
+    }
+
+
+questions()
